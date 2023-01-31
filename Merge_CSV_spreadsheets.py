9d9611c46449fea8220cdad4583b7c7d3ee9b6f7
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2023-01-31 15:01:49 -0300

    Python version

--- Merge_CSV_spreadsheets.py
+++ Merge_CSV_spreadsheets.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
-
 import os
 import pandas as pd
 
@@ -13,4 +7,3 @@ files = [file for file in os.listdir() if file.endswith('.csv')]
 df = pd.concat([pd.read_csv(f) for f in files])
 
 df.to_csv('./df.csv', sep=';', index=False)
-
